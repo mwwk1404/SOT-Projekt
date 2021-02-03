82421ed6780eb8ad274f2a48a1cc0e2df214d7c3
Moritz <moritz.klass@icloud.com>
2021-02-03 13:55:36 +0100

    Class-Diagramm hinzugefügt und Kundenklasse und Rechnung bereits erstellt.

---
+++ Kunde.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOT_Projekt_Fahrradladen
+{
+    class Kunde
+    {
+        string Vorname;
+        string Nachname;
+        int Hausnummer;
+        int PLZ;
+        string Stadt;
+    }
+}
