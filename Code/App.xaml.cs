60500dbdb0b05ae9f3fc2555024e224c673dd769
Moritz <moritz.klass@icloud.com>
2021-02-19 13:56:33 +0100

    SQL Script zu erstellung der Datenbank hinzugefügt und Ordnerstruktur geändert und erste Ansicht mit Databinding hinzugefügt

---
+++ Code/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Fahrradladen_18._02
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Datenbank/SQLQuery_Datenbank_erstellen.sql
@@ -0,0 +1,34 @@
+USE Master;
+GO
+
+CREATE DATABASE Verkauf;
+Use Verkauf;
+GO
+
+CREATE TABLE Artikel (
+Artikel_ID INT IDENTITY PRIMARY KEY,
+Artikel_Bezeichnung VARCHAR(100),
+Artikel_Preis DECIMAL);
+
+
+CREATE TABLE Artikelliste (
+Artikelliste_ID INT IDENTITY PRIMARY KEY,
+Gesamtpreis DECIMAL,
+Artikel_ID INT,
+FOREIGN KEY (Artikel_ID) REFERENCES Artikel(Artikel_ID));
+
+CREATE TABLE Rechnungen (
+Rechnungs_ID INT IDENTITY PRIMARY KEY,
+Rechnungsgesamtpreis DECIMAL,
+Artikelliste_ID INT,
+FOREIGN KEY (Artikelliste_ID) REFERENCES Artikelliste(Artikelliste_ID));
+
+INSERT INTO Artikel(Artikel_Bezeichnung, Artikel_Preis)
+VALUES
+('Fahrrad-Helm Grün', 20),
+('Fahrrad-Pumpe', 15),
+('Reifen-Schlauch', 7),
+('Fahrrad-Lampe', 5),
+('Trinkflasche', 3);
+
+
